5364dc61469ad13916836daca5ccffa89966122f
Jose Luis Morales W <joseluismorales.watanabe@externos-cl.cencosud.com>
2025-03-15 16:17:14 -0500

    initial setup workspace

--- apps/almacenar/src/app/app.service.ts
+++ apps/almacenar/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello API from fusionados' };
+    return { message: 'Hello API from almacenar' };
   }
 }

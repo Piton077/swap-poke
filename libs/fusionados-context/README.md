7ef10bf82d7a94893e3b7b4dcc246693bac0e271
Jose Luis Morales W <joseluismorales.watanabe@externos-cl.cencosud.com>
2025-03-16 17:30:45 -0500

    add bounded contexts

---
+++ libs/fusionados-context/README.md
@@ -0,0 +1,7 @@
+# fusionados-context
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test fusionados-context` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/fusionados-context/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'fusionados-context',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/fusionados-context',
+};
